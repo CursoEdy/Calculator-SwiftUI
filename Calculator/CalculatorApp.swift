8e27160f6f4d19b4187fbc3e774a8f804cefc413
edy alves <30.edyalves@gmail.com>
2024-11-10 12:18:47 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by ednardo alves on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
