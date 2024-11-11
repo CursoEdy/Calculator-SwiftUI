0de47d20dd59b50a873284d9afc85b11dbf4838a
edy alves <30.edyalves@gmail.com>
2024-11-11 18:59:14 -0300

    ButtonView e Enumarations

---
+++ Calculator/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  Calculator
+//
+//  Created by ednardo alves on 11/11/24.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
---
+++ Calculator/Enumerations.swift
@@ -0,0 +1,8 @@
+//
+//  Enumerations.swift
+//  Calculator
+//
+//  Created by ednardo alves on 10/11/24.
+//
+
+import Foundation
