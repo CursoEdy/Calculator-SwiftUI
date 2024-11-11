003908900464661cc70e7fe91acd8b19867b214c
edy alves <30.edyalves@gmail.com>
2024-11-11 19:00:44 -0300

    contents buttonViewe Enumarations

--- Calculator/Enumerations.swift
+++ Calculator/Enumerations.swift
@@ -1,8 +1,37 @@
-//
-//  Enumerations.swift
-//  Calculator
-//
-//  Created by ednardo alves on 10/11/24.
-//
-
 import Foundation
+
+enum Oparation {
+    case add, multiply, subtract, divide, equal, none
+}
+
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case multiply = "x"
+    case subtract = "-"
+    case divide = "/"
+    case equal = "="
+    case clear = "C"
+    case decimal = "."
+    case percent = "%"
+    case negative = "IMGplus.forwardslash.minus"
+    case undo = "IMGarrow.counterclockwise"
+}
+
+let enumOperators: [CalcButton] = [
+    .add, .multiply, .divide, .subtract
+]
+
+let multiplySymbol: String = CalcButton.multiply.rawValue
+let divisionSymbol: String = CalcButton.divide.rawValue
+
+let operators: String = "+-\(multiplySymbol)\(divisionSymbol)"
