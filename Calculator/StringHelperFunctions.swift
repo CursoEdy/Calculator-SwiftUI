9f53dbd974b8c547209bda2c68eefe69ddb81a72
edy alves <30.edyalves@gmail.com>
2024-11-15 14:58:00 -0300

    implements stringHelperFunction

---
+++ Calculator/StringHelperFunctions.swift
@@ -0,0 +1,33 @@
+//
+//  StringHelperFunctions.swift
+//  Calculator
+//
+//  Created by ednardo alves on 15/11/24.
+//
+
+import Foundation
+
+
+//retorna ultimo caracter se existir
+func getLastChar(str: String) -> String{
+//    if str.isEmpty {
+//        return ""
+//    }else {
+//        return String(str.last!)
+//    }
+    
+    return str.isEmpty ? "" : String(str.last!)
+}
+
+//retorna verdadeiro se um caracter str for igual a char
+func lastCharacterIsEqualTo(str: String, char: String) -> Bool{
+    return getLastChar(str: str) == char
+}
+
+func formatResult(val: Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    numberFormatter.maximumFractionDigits = 16
+    
+    return numberFormatter.string(from: NSNumber(value: val)) ?? "0"
+}
